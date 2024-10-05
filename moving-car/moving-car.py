72b6e59db953236c5a732ce36238a7e799ca7aea
sathyan4321 <sathyanr.4321@gmail.com>
2024-10-05 20:04:07 +0530

    first commit

---
+++ moving-car/moving-car.py
@@ -0,0 +1,9 @@
+# This is fully education purposed project.
+# It is not intended for commercial use.
+# Author : Sathya Narayanan.R
+# Date of Creation : 05-10-2024
+# Date of Last Modification : 05-10-2024
+# Country : India
+
+
+
